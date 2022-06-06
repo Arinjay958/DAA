a99a72e1d1f08600dbf57603e319bd56b38d3f84
Arinjay Aggarwal <103314061+Arinjay958@users.noreply.github.com>
2022-06-06 21:49:31 +0530

    Update Week6Ques1.cpp

--- Week6Ques1.cpp
+++ Week6Ques1.cpp
@@ -1,41 +1,44 @@
 #include<iostream>
-#include <bits/stdc++.h>
-using namespace std;
-int main()
-{
-    int n, u, v, m;
-    cout<<"Enter the number of vertices "<<"\n";
-    cin>>n;
-    int arr[n][n];
-    for(int i=0;i<n+1;i++)
-    {
-       
-        for(int j=0;j<n+1;j++)
-        {
-            arr[i][j] = 0;
-        }
-    }
-    cout<<"Enter the number of edges "<<"\n";
-    cin>>m;
-    for(int i=0;i<m;i++)
-    {
-        cin>>u>>v;
-        arr[u][v] = 1;
-        arr[v][u] = 1;
-    }
-    // int src, dest;
-    // cout<<"Enter the source vertex ";
-    // cin>>src;
-    // cout<<"Enter the destination vertex ";
-    // cin>>dest;
-    
+#include<vector>
 
-    for(int i=1;i<=n;i++)
-    {
-        for(int j=1;j<=n;j++)
-        {
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<"\n";
-    }
+using namespace std; 
+
+bool dfs(int src,vector<int>&vis,vector<vector<int>>&v,int n,int dest)
+{
+     vis[src]=1;
+     if(src==dest){
+         return true;
+     }
+     for(int i=1;i<n+1;i++)
+     {
+         if(vis[i]!=1 && v[src][i]!=0)
+         {
+             return dfs(i,vis,v,n,dest);
+         }
+     }
+         return false;
+}
+int main(){
+int n;
+cout<<"enter the no. of nodes"<<endl;
+cin>>n;
+vector<vector<int>>v(n+1,vector<int>(n+1,0));
+
+ for(int i=1;i<n+1;i++)
+ {
+     for(int j=1;j<n+1;j++)
+     cin>>v[i][j];
+ }
+ vector<int>vis(n+1);
+ int src,dest;
+ cout<<"Enter two vertex to find path: ";
+ cin>>src>>dest;
+     bool flag=dfs(src,vis,v,n,dest);
+if(flag==true){
+    cout<<"Path exist";
+}
+else{
+    cout<<"Path does not exist";   
+}
+return 0;
 }
